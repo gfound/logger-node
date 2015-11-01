bebcf579454f321ced943dc1c54a3ef9e0763292
金磊 <lordking@163.com>
2015-11-01 17:28:03 +0800

    增加数据库写入

--- example/logger-config.js
+++ example/logger-config.js
@@ -1,9 +1,22 @@
 module.exports = {
-	name: process.env.LOG_NAME || 'logger-sample',
-
-	loggerService: process.env.LOGGER_SERVICE || '',
-
-	stdout: process.env.LOG_STOUT || true,
+	name: process.env.LOG_NAME || 'loggerSample',
 	json: process.env.LOG_JSON || false,
-	level: process.env.LOG_LEVEL || 'trace'
+	level: process.env.LOG_LEVEL || 'trace',
+
+	//输出到控制台
+	stdout: process.env.LOG_STOUT || true,
+
+	//输出到日志服务器
+	service: process.env.LOG_SERVICE || false,
+	serviceUrl: process.env.LOG_SERVICE_URL || '',
+	
+	//输出到mongodb数据库
+	db: process.env.LOG_DB || true,
+	dbConfig: {
+		host: process.env.LOG_DB_HOST || '127.0.0.1',
+		port: process.env.LOG_DB_PORT || 27017,
+		user: process.env.LOG_DB_USER || '',
+		password: process.env.LOG_DB_PASSWORD || '',
+		database: process.env.LOG_DB_NAME || 'logs'
+	}
 }
