7679e5218b9380bd084a3e12cd5bc59abfccec75
金磊 <lordking@163.com>
2016-02-22 15:33:24 +0800

    no message

--- example/logger-config.js
+++ example/logger-config.js
@@ -1,17 +1,25 @@
 module.exports = {
 	name: process.env.LOG_NAME || 'loggerSample',
-	level: process.env.LOG_LEVEL || 'trace',
+	level: process.env.LOG_LEVEL || 'info',
 
 	//输出到控制台
-	stdout: process.env.LOG_STOUT || true,
-	json: process.env.LOG_JSON || false,
+	stdout: process.env.LOG_STOUT || false,
+	json: process.env.LOG_JSON || true,
+
+	//输出到Kafka HTTP地址
+	kafkaHTTP: process.env.LOG_KAFKA_HTTP || false,
+	kafkaUrl: process.env.LOG_KAFKA_URL || 'http://localhost:8082/topics/test',
 
 	//输出到Kafka
 	kafka: process.env.LOG_KAFKA || true,
-	kafkaUrl: process.env.LOG_KAFKA_URL || 'http://localhost:8082/topics/test',
-	
+	kafkaServer: {
+		host: process.env.KAFKA_HOST || '192.168.0.176',
+		port: process.env.KAFKA_PORT || 9092,
+		topic:process.env.KAFKA_TOPIC || 'calllog'
+	},
+
 	//输出到mongodb数据库
-	db: process.env.LOG_DB || true,
+	db: process.env.LOG_DB || false,
 	dbConfig: {
 		host: process.env.LOG_DB_HOST || '127.0.0.1',
 		port: process.env.LOG_DB_PORT || 27017,
--- example/logger-sample.js
+++ example/logger-sample.js
@@ -1,13 +1,27 @@
-
+var os = require("os")
 
 var loggerConfig = require('./logger-config');
 var loggerNode = require('../lib/logger-node');
 
-var logger1 = loggerNode.createLogger(loggerConfig);
-logger1.info('hi');
+var logger = loggerNode.createLogger(loggerConfig);
+
+logger.info('hi');
 
 var logger2 = loggerNode.getLogger();
+
 function hello() {
-	logger2.debug('hi');
+  logger2.debug('hi');
 }
 hello();
+
+var callLog = {};
+callLog.report = true;
+callLog.content = "test";
+callLog.from = "admin"
+callLog.to = "189"
+callLog.msgType = "audio";
+callLog.timestamp = "2016-02-22 11:34:34.854";
+callLog.action = "receive";
+callLog.module = "callServer-" + os.hostname();
+
+logger.info({report:true}, callLog)
